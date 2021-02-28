663ed55552efe134a124616a8b261b1ce2f25056
Kiki Rizki <kkrzkrk@gmail.com>
2021-02-28 22:46:07 +0700

    Add minimum vue home page

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is Home Page!'
+    return render_template('index.html')
