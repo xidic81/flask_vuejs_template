1d076598ae3994fcdaa60d9c5635d452e5b9374e
xidic81 <sidikmul@gmail.com>
2021-03-13 13:36:09 +0700

    bind element attributes

--- static/index.js
+++ static/index.js
@@ -1,8 +1,8 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        msg: 'Hello Vue!'
+        msg: 'You loaded this page on ' + new Date().toLocaleString()
     }
 });
 
-app.msg = 'I have changed the data!';
+// app.msg = 'I have changed the data!';
